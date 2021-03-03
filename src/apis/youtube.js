81cc4c4940ab145cda53a1c1773e5a73a1437dc1
tomppk <tomi.kivioja@gmail.com>
2021-03-03 19:25:05 +0200

    small change

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const KEY = "AIzaSyCOuMegtWhLd3mbq86HvpdpxPy90wcCzYk";
-
 // Set up axios default params
 // baseURL to Youtube API and default request
 // parameters required by Youtube API in their documentation
@@ -11,6 +9,6 @@ export default axios.create({
     part: "snippet",
     type: "video",
     maxResults: 5,
-    key: KEY,
+    key: process.env.REACT_APP_KEY,
   },
 });
